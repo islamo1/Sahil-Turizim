bb31f0d688f2f38e57096f2c59edfa262d6eb300
Islam Razzo <islam.razzouk.2004@gmail.com>
2025-05-23 17:46:32 +0300

    Switch to PostgreSQL with Railway env vars

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
+    
 }
+
 
 
 # Password validation
